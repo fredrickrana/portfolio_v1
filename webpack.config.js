f34dc955aa1d18c02f9f51c88e593408a1d3198f
Fredrick Rana <fredrickrana01@gmail.com>
2023-02-10 11:24:37 +0000

    test

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,26 @@
+
+
+require('dotenv/config');
+const path = require('path');
+const webpack = require('webpack');
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+
+const clientPath = path.join(__dirname, 'client');
+const serverPublicPath = path.join(__dirname, 'server', 'public');
+
+const isDevelopment = process.env.NODE_ENV === 'development';
+
 module.exports = {
+  mode: process.env.NODE_ENV,
+  entry: [
+    clientPath,
+    isDevelopment && 'webpack-hot-middleware/client?timeout=1000'
+  ].filter(Boolean),
   resolve: {
     extensions: ['.js', '.jsx']
+  },
+  output: {
+    path: serverPublicPath
   },
   module: {
     rules: [
@@ -10,15 +30,34 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
+            presets: [
+              '@babel/preset-env'
+            ],
             plugins: [
-              '@babel/plugin-transform-react-jsx'
-            ]
+              '@babel/plugin-transform-react-jsx',
+              isDevelopment && 'react-refresh/babel'
+            ].filter(Boolean)
           }
         }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
       }
     ]
   },
+  stats: 'minimal',
   performance: {
     hints: false
-  }
+  },
+  devtool: isDevelopment ? 'cheap-module-source-map' : 'source-map',
+  plugins: [
+    new webpack.EnvironmentPlugin([]),
+    isDevelopment && new ReactRefreshWebpackPlugin(),
+    isDevelopment && new webpack.NoEmitOnErrorsPlugin(),
+    isDevelopment && new webpack.HotModuleReplacementPlugin()
+  ].filter(Boolean)
 };
